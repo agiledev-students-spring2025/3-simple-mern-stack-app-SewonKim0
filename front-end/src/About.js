5c7562b3a457157fb03da9e82db76ccb719fbca3
SewonKim0 <sewonkim0black@gmail.com>
2025-02-12 23:41:44 -0500

    React: fetching of imageUrl & Paragraphs complete

--- front-end/src/About.js
+++ front-end/src/About.js
@@ -1,17 +1,30 @@
-import { useState, useEffect } from 'react';
-import './About.css';
+import { useState, useEffect } from 'react'
+import './About.css'
 
 export default function About() {
+    const [messages, setMessages] = useState([])
+    const [imageUrl, setImageUrl] = useState(null)
+
+    // fetch all data from server
+    useEffect(() => {
+        fetch('http://localhost:5002/about-us')
+            .then(response => response.json())
+            .then(data => {
+                setMessages(data.paragraphs)
+                setImageUrl(data.imageUrl)
+            })
+            .catch(err => console.error(err))
+    }, [])
+
     return <div>
         <h1 className="title"> About Us </h1>
 
-        <img src="./images/sewon.jpg" className="image" />
+        <img src={ imageUrl } className="image" />
 
         <div className="paragraphs">
-            <p> p </p>
-            <p> p </p>
-            <p> p </p>
-            <p> p </p>
+            { messages.map((message) => {
+                return <p> { message } </p>
+            }) }
         </div>
     </div>
 }
